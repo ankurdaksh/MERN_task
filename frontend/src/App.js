56e0a46fcf81193e085f8177d6c65d152e6b1659
ankurdaksh <ankurdaksh177@gmail.com>
2024-03-07 08:27:26 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CandidateForm from "./CandidateForm";
+
+function App() {
+  return (
+    <>
+      <CandidateForm />
+    </>
+  );
+}
+
+export default App;
